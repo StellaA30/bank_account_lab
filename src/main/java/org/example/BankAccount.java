00ca8e50fe5f6e5d9510884ed459175050fa3ab3
Stella Annor <stella.annor@icloud.com>
2023-03-29 00:10:14 +0100

    initial commit

---
+++ src/main/java/org/example/BankAccount.java
@@ -0,0 +1,112 @@
+package org.example;
+
+public class BankAccount {
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private int accountNumber;
+    private double balance;
+    private String accountType;
+    private double overdraft;
+
+//    constructor
+
+    public BankAccount(String firstName, String lastName, String dateOfBirth, int accountNumber, String accountType){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.balance = 0;
+        this.overdraft = -300;
+    }
+
+//    deposit method
+    public void deposit(double amount){
+        this.balance += amount;
+        System.out.println("Your new balance is " + this.balance );
+    }
+
+//    withdrawal method
+    public void withdrawal(double amount){
+        if(this.balance - amount < this.overdraft){
+            System.out.println("You cannot withdraw money now, you have reached your overdraft limit!!");
+        } else {
+            this.balance -= amount;
+            System.out.println("Your new balance is " + this.balance);
+        }
+    }
+
+// payInterest method
+
+    public double payInterest(double interestRate){
+        if(this.accountType.toLowerCase() == "current"){
+            this.balance += this.balance * (interestRate/100);
+//           assumes we add 2% extra if it's a savings account
+        } else if(this.accountType.toLowerCase() == "savings"){
+            this.balance += this.balance * ((2 + interestRate)/100);
+        }
+        return this.balance;
+    }
+
+//getters
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    public String getLastName(){
+        return this.lastName;
+    }
+
+    public String getDateOfBirth(){
+        return this.dateOfBirth;
+    }
+
+    public int getAccountNumber(){
+        return this.accountNumber;
+    }
+
+    public double getBalance(){
+        return this.balance;
+    }
+
+    public String getAccountType(){
+        return this.accountType;
+    }
+
+    public double getOverdraft(){
+        return this.overdraft;
+    }
+
+//    setters
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public void setDateOfBirth(String dateOfBirth){
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setAccountNumber(int accountNumber){
+        this.accountNumber = accountNumber;
+    }
+
+    public void setBalance(double balance){
+        this.balance = balance;
+    }
+
+    public void setAccountType(String accountType){
+        this.accountType = accountType;
+    }
+
+    public void setOverdraft(double overdraft){
+        this.overdraft = overdraft;
+    }
+
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,40 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BankAccount person = new BankAccount("Stella", "Annor", "30.05.97", 12345, "current");
+//        BankAccount person2 = new BankAccount("John", "Snow", "29.03.2023", 54321,);
+
+
+//        checking the setters
+        person.setFirstName("Florian");
+        person.setLastName("Adams");
+        person.setDateOfBirth("13.04.2001");
+        person.setBalance(250);
+        person.setAccountNumber(696926);
+        person.setAccountType("savings");
+        person.setOverdraft(-300);
+
+//        checking the getters
+        System.out.println(person.getFirstName());
+        System.out.println(person.getLastName());
+        System.out.println(person.getDateOfBirth());
+        System.out.println(person.getBalance());
+        System.out.println(person.getAccountNumber());
+        System.out.println(person.getAccountType());
+        System.out.println(person.getOverdraft());
+
+//        checking the methods
+        int withdrawalAmount = 600;
+        person.withdrawal(withdrawalAmount);
+
+        int depositAmount = 100;
+        person.deposit(depositAmount);
+
+//        fixed interest rate of 1.5% for current account
+        double interestRate = 1.5;
+        System.out.println(person.payInterest(interestRate));
+
+    }
+}
